0a1e8031ee01c4f5adba6df64d4b50d3f331463a
COMPUS029\daw1 <agutierrezp18@educantabria.es>
2026-05-12 09:52:00 +0200

    Estructura inicial de PacienteDAO con método getAllPacientes y Builder

---
+++ Dentify/src/main/java/com/example/dentify/dao/PacienteDAO.java
@@ -0,0 +1,10 @@
+package com.example.dentify.dao;
+
+public class PacienteDAO {
+
+    //CREAR, LEER, ACTUALIZAR, BORRAR
+    //getAllPacientes(): Este es el que ya hemos empezado. Lo necesitas para cargar la lista completa en cuanto se abre la ventana.
+    //getPacientesByFilter(String texto): Usará una sentencia SQL con varios OR y el operador LIKE para buscar en las tres columnas a la vez.
+
+
+}
